bf5ea4a7e970693ccaccc0381c154c9a611f18f1
otash16 <otajonqutbiddinov1@gmail.com>
2024-09-08 15:30:42 +0500

    feat: create project standards

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -4,7 +4,8 @@ import MemberService from "../models/Member.service";
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
     try{
-        res.send("Home Page")
+        console.log("goHome");
+        res.send("Home Page");
     }catch(err){
         console.log("Error on Home Page", err);
     }
@@ -12,6 +13,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 
 restaurantController.getLogin = (req: Request, res: Response) => {
     try{
+        console.log("getLogin");
         res.send("Login Page")
     }catch(err){
         console.log("Error on Login Page", err);
@@ -20,6 +22,7 @@ restaurantController.getLogin = (req: Request, res: Response) => {
 
 restaurantController.getSignup = (req: Request, res: Response) => {
     try{
+        console.log("getSignup");
         res.send("Signup Page")
     }catch(err){
         console.log("Error on Signup Page", err);
