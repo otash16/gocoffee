fc14bc39709890841856e88cf1ac9c13e5de15e7
otash16 <otajonqutbiddinov1@gmail.com>
2024-09-12 02:43:11 +0500

    feat: develop restaurant process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,7 +1,7 @@
 import { T } from "../libs/types/common"
 import {Request, Response} from "express";
 import MemberService from "../models/Member.service";
-import { MemberInput } from "../libs/types/member";
+import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
@@ -22,12 +22,19 @@ restaurantController.getLogin = (req: Request, res: Response) => {
     }
 }
 
-restaurantController.processLogin = (req: Request, res: Response) => {
+restaurantController.processLogin = async (req: Request, res: Response) => {
     try{
         console.log("processLogin");
-        res.send("Done")
+        console.log("req.body: ",req.body);
+        const input: LoginInput = req.body;
+
+        const memberService = new MemberService();
+        const result = await memberService.processLogin(input)
+
+        res.send(result);
     }catch(err){
         console.log("Error on Login Page", err);
+        res.send(err);
     }
 }
 
--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -4,36 +4,42 @@ export enum HttpCode {
     NOT_MODIFIED = 304,
     BAD_REQUEST = 400,
     UNAUTHORIZED = 401,
-    FORBIDDEN = 403,
+    FORBIDDIN = 403,
     NOT_FOUND = 404,
-    INTERNAL_SERVER_ERROR 
-}    
-
-
-export enum Message {
-    SOMETHING_WENT_WRONG = "Something went wrong!",
-    NO_DATA_FOUND = "No data is found!",
-    CREATE_FAILED = "Create is failed!",
-    UPDATE_FAILED = "Update is failed!",
-
+    INTERNAL_SERVER_ERROR = 500,
+  }
+  
+  export enum Message {
+    SOMETHING_WENT_WRONG = "Something went wrong",
+    NO_DATA_FOUND = "No data is found",
+    CREATED_FAILED = "Create is failed",
+    UPDATE_FAILED = "Update is failed",
+  
+    USED_NICK_PHONE = "you are inserting already used nick or phone!",
     NO_MEMBER_NICK = "No member with that member nick!",
-    TOKEN_CREATION_FAILED = "Token creation error!",
     BLOCKED_USER = "You have been blocked, contact restaurant!",
-    USED_NICK_PHONE = "You are inserting already used nick or phone",
-    WRONG_PASSWORD = "Wrong password, please, try again!",
-    NOT_AUTHENTICATED = "You are not authenticated, Please login first",
-}
-
-
-class Errors extends Error {
-     public code: HttpCode;
-     public message: Message;
-
-     constructor(statusCode: HttpCode, statusMessage: Message){
-        super();
-        this.code = statusCode;
-        this.message = statusMessage;
-     }
-}
-
-export default Errors;
+    WRONG_PASSWORD = "Wrong password, please try again!",
+    NOT_AUTHENTICATED = "You are not Authoticates, login first!",
+    TOKEN_CREATION_FAILED = "Token creation error",
+  }
+  
+  class Errors extends Error {
+    static OK(OK: any) {
+      throw new Error("Method not implemented.");
+    }
+    public code: HttpCode;
+    public message: Message;
+  
+    static standard = {
+      code: HttpCode.INTERNAL_SERVER_ERROR,
+      message: Message.SOMETHING_WENT_WRONG,
+    };
+  
+    constructor(statusCode: HttpCode, statusMessage: Message) {
+      super();
+      this.code = statusCode;
+      this.message = statusMessage;
+    }
+  }
+  
+  export default Errors;
